ce551f3b8062ff54e0a3695743eee00cc06c4cb8
Ishaan Jain <ishaan160799@gmail.com>
2020-09-12 19:36:02 +0530

    internal:content intent set

--- app/src/main/java/com/ironclad/notificationpractice/InternalNotificationActivity.kt
+++ app/src/main/java/com/ironclad/notificationpractice/InternalNotificationActivity.kt
@@ -1,5 +1,7 @@
 package com.ironclad.notificationpractice
 
+import android.app.PendingIntent
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.app.NotificationCompat
@@ -16,10 +18,19 @@ class InternalNotificationActivity : AppCompatActivity() {
         btnSendOnChannel1.setOnClickListener {
             val title = etTitle.text.toString()
             val message = etMessage.text.toString()
+
+            val activityIntent = Intent(this, InternalNotificationActivity::class.java)
+            val contentIntent = PendingIntent.getActivity(
+                this,
+                1605,
+                activityIntent,
+                0
+            )
             val notification = NotificationCompat.Builder(this, getString(R.string.channel1))
                 .setSmallIcon(R.drawable.ic_baseline_notifications_24)
                 .setContentTitle(title)
                 .setContentText(message)
+                .setContentIntent(contentIntent)
                 .setCategory(NotificationCompat.CATEGORY_MESSAGE)
                 .build()
 
