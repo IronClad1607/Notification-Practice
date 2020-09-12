7bc91d7b9300843363abf09c2ba529605ccc0379
Ishaan Jain <ishaan160799@gmail.com>
2020-09-12 14:29:20 +0530

    firebase notification done!

--- app/src/main/java/com/ironclad/notificationpractice/MainActivity.kt
+++ app/src/main/java/com/ironclad/notificationpractice/MainActivity.kt
@@ -2,10 +2,24 @@ package com.ironclad.notificationpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.iid.FirebaseInstanceId
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        FirebaseInstanceId.getInstance().instanceId
+            .addOnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    Log.d("PUI", "GetInstance Id Failed", task.exception)
+                    return@addOnCompleteListener
+                }
+
+                val token = task.result?.token
+
+                Log.d("PUI", "Token: $token")
+            }
     }
 }
--- app/src/main/java/com/ironclad/notificationpractice/services/FCMService.kt
+++ app/src/main/java/com/ironclad/notificationpractice/services/FCMService.kt
@@ -1,7 +1,23 @@
 package com.ironclad.notificationpractice.services
 
+import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
 class FCMService : FirebaseMessagingService() {
+    override fun onMessageReceived(p0: RemoteMessage) {
+        Log.d("PUI", "From:${p0.from}")
 
+        if (p0.data.isNotEmpty()) {
+            Log.d("PUI", "Message data Payload: ${p0.data}")
+        }
+
+        p0.notification?.let {
+            Log.d("PUI", "Message Notification Body: ${it.body}")
+        }
+    }
+
+    override fun onNewToken(p0: String) {
+        Log.d("PUI", "Refreshed Token: $p0")
+    }
 }
