358785939562dd3bcd079eca9dc48ed691156218
Ishaan Jain <ishaan160799@gmail.com>
2020-09-12 18:45:03 +0530

    internal basic done!

--- app/src/main/java/com/ironclad/notificationpractice/InternalNotificationActivity.kt
+++ app/src/main/java/com/ironclad/notificationpractice/InternalNotificationActivity.kt
@@ -2,10 +2,40 @@ package com.ironclad.notificationpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import kotlinx.android.synthetic.main.activity_internal_notification.*
 
 class InternalNotificationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_internal_notification)
+
+        val notificationManager = NotificationManagerCompat.from(this)
+
+        btnSendOnChannel1.setOnClickListener {
+            val title = etTitle.text.toString()
+            val message = etMessage.text.toString()
+            val notification = NotificationCompat.Builder(this, getString(R.string.channel1))
+                .setSmallIcon(R.drawable.ic_baseline_notifications_24)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .build()
+
+            notificationManager.notify(1, notification)
+        }
+
+        btnSendOnChannel2.setOnClickListener {
+            val title = etTitle.text.toString()
+            val message = etMessage.text.toString()
+            val notification = NotificationCompat.Builder(this, getString(R.string.channel2))
+                .setSmallIcon(R.drawable.ic_baseline_notifications_active_24)
+                .setContentTitle(title)
+                .setContentText(message)
+                .build()
+
+            notificationManager.notify(2, notification)
+        }
     }
 }
